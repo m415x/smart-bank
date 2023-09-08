3c3aa69af2f65b5146c2404f05931f8f01bb4949
Cristian Lahoz <lahozcristian@gmail.com>
2023-09-08 15:49:20 -0300

    clase 01

--- src/Components/Title/index.jsx
+++ src/Components/Title/index.jsx
@@ -1,6 +1,9 @@
-import React from "react";
 
-const Title = ({ children }) => {
-  return <h1 className="title">{children}</h1>;
-};
-export default Title;
+import styled from "styled-components"
+
+const Title = styled.h1`
+  color: gray;
+  padding: 25px 0;
+`
+
+export default Title
